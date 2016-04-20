093b2282e9f3a5cae22a63d183be72106760bdcb
pachif <pachif81@yahoo.com.ar>
2016-04-20 10:18:02 -0300

    integrating the app with the site, also adding some Leafletjs library and start working on that path

---
+++ PuppyApp/DTOS/HistoryGeoJsonDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuppyApp.DTOS {
+    public class HistoryGeoJsonDTO {
+        public string Type { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
